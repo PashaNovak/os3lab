6a232be5e8e3dbbbd8ec06c644168e84b0d8587f
Андрей Турцевич <andrey.turcevich@mail.ru>
2016-10-25 21:08:09 +0300

    created process p2

--- OS3LB/OS3LB.cpp
+++ OS3LB/OS3LB.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <Windows.h>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ int __cdecl wmain(int argc, wchar_t *argv[], wchar_t *envp[]);
 
 int create_number()
 {
-	int number = rand();
+	int number = rand() % 9 + 1;
 	return number;
 }
 
@@ -20,21 +21,19 @@ void writeToClipboard(char*str, int n) {
 	memcpy(GlobalLock(hMem), str, len);
 	GlobalUnlock(hMem);
 	OpenClipboard(0);
-	EmptyClipboard();
 	SetClipboardData(CF_TEXT, hMem);
 	CloseClipboard();
 }
 
-void readFromClipboard() {
+char readFromClipboard() {
+	char* data;
 	if (OpenClipboard(NULL))
 	{
 		int d = 1;
-		char* data = (char*)GetClipboardData(CF_TEXT);
-		for (int i = 0; i < 1; ++i) {
-			printf("Number:%c\n", data[i]); d++;
-		}
+		data = (char*)GetClipboardData(CF_TEXT);
 		CloseClipboard();
 	}
+	return *data;
 }
 
 int wmain(int argc, wchar_t * argv[], wchar_t * envp[])
@@ -50,21 +49,43 @@ int wmain(int argc, wchar_t * argv[], wchar_t * envp[])
 	saThread.nLength = sizeof(saThread);
 	saThread.lpSecurityDescriptor = NULL;
 	saThread.bInheritHandle = FALSE;
+	char buf[10];
+	//for (int i = 0; i < 10; i++) {
+		sprintf_s(buf, "%d", create_number());
+		writeToClipboard(buf, strlen(buf));
+	//}
 	lstrcpy(szPath, TEXT("C:\\Windows\\system32\\cmd.exe"));
-	BOOL fSuccess = CreateProcess(NULL, szPath, &saProcess, &saThread,
+	BOOL processP1 = CreateProcess(NULL, szPath, &saProcess, &saThread,
 		FALSE, 0, NULL, NULL, &si, &piProcessP1);
-	if (fSuccess) {
+	if (processP1) {
 		CloseHandle(piProcessP1.hThread); //Закрываем описатель потока, как только в нем необходимость отпадет
 		cout << "Process p1 was created successfully!" << endl;
-		char buf[10];
-		for (int i = 0; i < 10; i++) {
-			sprintf_s(buf, "%d", create_number());
-			writeToClipboard(buf, strlen(buf));
-			readFromClipboard();
-		}
+		char* data = (char*)readFromClipboard();
+		//for (int i = 0; i < 10; ++i) {
+			printf("Number:%c\n", data);
+		//}
 		GetExitCodeProcess(piProcessP1.hProcess, &dwExitCode); // Дочерний процесс завершился, получаем код его завершения
 		CloseHandle(piProcessP1.hProcess); // Закрываем описатель процесса, как только необходимость в нем отпадет
 	}
+	lstrcpy(szPath, TEXT("C:\\Program Files (x86)\\Notepad++\\notepad++.exe"));
+	BOOL processP2 = CreateProcess(NULL, szPath, &saProcess, &saThread,
+		FALSE, 0, NULL, NULL, &si, &piProcessP2);
+	if (processP2) {
+		CloseHandle(piProcessP2.hThread); //Закрываем описатель потока, как только в нем необходимость отпадет
+		cout << "Process p2 was created successfully!" << endl;
+		const char* data = (const char*)readFromClipboard();
+		char s[10] = { (char)data };
+		FILE * ptrFile = fopen("numbers.txt", "w");
+		if (ptrFile != NULL)
+		{
+			fputs(s, ptrFile);
+			fclose(ptrFile);
+		}
+		system("pause");
+		GetExitCodeProcess(piProcessP2.hProcess, &dwExitCode); // Дочерний процесс завершился, получаем код его завершения
+		CloseHandle(piProcessP2.hProcess); // Закрываем описатель процесса, как только необходимость в нем отпадет
+	}
+
 	return 0;
 }
 
